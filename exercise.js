1b7e594558b3ddea44226dee0fc05e8fb4e94a15
Ann Aiko Yamauchi <avonpalo@Anns-MacBook-Pro.local>
2017-02-06 23:22:45 -0800

    js-stringMethods

--- exercise.js
+++ exercise.js
@@ -8,6 +8,8 @@
 /*1. Declare a variable named `howManyLetters` and find out how many letters are in the given string below. Console.log your result.*/
 
 var longestPlaceName = "Taumatawhakatangihangakoauauotamateaturipukakapiki- maungahoronukupokaiwhenuakitanatahu";  //Yes, this is a real place located in Porangahau, Central Hawke's Bay.
+var howManyLetters = longestPlaceName.length;
+console.log (howManyLetters);
 
 
 
@@ -16,12 +18,16 @@ var longestPlaceName = "Taumatawhakatangihangakoauauotamateaturipukakapiki- maun
 where x represents the value at `longestPlaceName` and y represents the value at `howManyLetters`*/
 
 
+
+console.log (longestPlaceName + " is the longest place name in the world and has " + howManyLetters + " letters in its name.";
+
 /*The string.charAt() method returns the character at the specified index (position).*/
 
 /*2. Declare a variable named `fifthLetter` and find out which letter is in the 5th position in the string below. Console.log your result.*/
 
 var iDidntKnow = "The national animal of Scotland is the Unicorn";
-
+var fifthLetter  = iDidntKnow.charAt(5);
+console.log(fifthLetter);
 
 
 /*The indexOf() method returns the index (position) of the first occurence of a specified text in a string.*/
@@ -29,6 +35,8 @@ var iDidntKnow = "The national animal of Scotland is the Unicorn";
 /*3. Declare a variable named `worldLocator` and find the index (position) of the word "world" in the string below. Console.log your result.*/
 
 var randomFact = "All pandas in the world are on loan from China.";
+var worldLocator = randomFact.indexOf("world");
+console.log(worldLocator);
 
 
 
@@ -37,7 +45,8 @@ var randomFact = "All pandas in the world are on loan from China.";
 /*4. Declare a new variable named `scissorHand` that will generate a new string "are afraid" from the existing string below. Console.log your result.*/
 
 var edward = "People are afraid of me because I am different";
-
+var scissorHand = edward.slice(7, 17);
+console.log(scissorHand;)
 
 
 /*The replace() method replaces a specified value with another value in a string.*/
@@ -45,7 +54,8 @@ var edward = "People are afraid of me because I am different";
 /*5. Declare a new variable named `theSifu` that will change the current string from "In order to taste my cup of water you must first fill your cup." to "In order to taste my cup of water you must first empty your cup."; Console.log your result.*/
 
 var grasshopper = "In order to taste my cup of water you must first fill your cup.";
-
+var theSifu = grasshopper.replace("fill", "empty");
+console.log(theSifu);
 
 
 /*The toLowerCase() method converts the characters in a string to lower case.*/
@@ -69,6 +79,8 @@ var bandName = "the beatles";
 /*8. Declare a variable named `oreoList` that will convert the string below into an array of strings. Console.log your result.*/
 
 var oreos = "red velvet, cookie dough, peanut butter, banana split, birthday cake";
+var oreoList = oreos.split(",");
+console.log(oreoList);
 
 
 
@@ -77,9 +89,12 @@ var oreos = "red velvet, cookie dough, peanut butter, banana split, birthday cak
 /*9. Declare a variable named `favDrink` and extract "gin" from the string below. Console.log your result.*/
 
 var drinkMenu = "rum, gin, vodka, kool-aid, haterade";
+var favDrink = drinkMenu.substr(5, 3);
+console.log(favDrin);
 
-
-
+/* You can also use:
+var favDrink = drinkMenu.substring(5, 8);
+*/
 
 
 
